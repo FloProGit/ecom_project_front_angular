b73a5ca52b16303b2b4524589a75fc12eadc91dc
florian <sudreflorianpro@gmail.com>
2023-05-30 14:25:41 +0200

    fix(window): resize nav menu fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,62 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
 import {animate, state, style, trigger, transition, query} from "@angular/animations";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  animations:[
-    trigger('burgerButtonSpan1',[
-    state('open', style({ transform: 'rotate(-135deg)' })),
-      transition( 'close => open',animate('0.3s ease-out')),
-      state('close', style({ transform: 'translateY(7px)' })),
-      transition( 'open => close',animate('0.3s ease-out')),
-  ]),
-    trigger('burgerButtonSpan2',[
-      state('open', style({ transform: 'rotate(135deg)' })),
-      transition( 'close => open',animate('0.3s ease-out')),
-      state('close', style({ transform: 'translateY(-7px)' })),
-      transition( 'open => close',animate('0.3s ease-out')),
+  animations: [
+    trigger('burgerButtonSpan1', [
+      state('open', style({transform: 'rotate(-135deg)'})),
+      transition('close => open', animate('0.3s ease-out')),
+      state('close', style({transform: 'translateY(7px)'})),
+      transition('open => close', animate('0.3s ease-out')),
     ]),
-    trigger('burgerButtonSpan3',[
-      state('open', style({ opacity: 0 })),
-      transition( 'close => open',animate('0.15s ease')),
-      state('close', style({ opacity: 1 })),
-      transition( 'open => close',animate('0.3s ease')),
+    trigger('burgerButtonSpan2', [
+      state('open', style({transform: 'rotate(135deg)'})),
+      transition('close => open', animate('0.3s ease-out')),
+      state('close', style({transform: 'translateY(-7px)'})),
+      transition('open => close', animate('0.3s ease-out')),
     ]),
-    trigger('burgerMenu',[
-      state('open', style({ transform:'translateX(0)',opacity:1 })),
-      transition( 'close => open',animate('0.3s ease')),
-      state('close', style({transform:'translateX(-100%)',opacity:0 })),
-      transition( 'open => close',animate('0.3s ease')),
+    trigger('burgerButtonSpan3', [
+      state('open', style({opacity: 0})),
+      transition('close => open', animate('0.15s ease')),
+      state('close', style({opacity: 1})),
+      transition('open => close', animate('0.3s ease')),
+    ]),
+    trigger('burgerMenu', [
+      state('open', style({transform: 'translateX(0)', opacity: 1})),
+      transition('close => open', animate('0.3s ease')),
+      state('close', style({transform: 'translateX(-100%)', opacity: 0})),
+      transition('open => close', animate('0.3s ease')),
     ])
   ]
 })
 
-// transition('open => close',[
-//   query('.hamburger,:nth-child(1)',animate(1000, style({ transform: 'translateX(50px)' }))),
-//   query('.hamburger,:nth-child(1)',animate(1000, style({ transform: 'translateX(50px)' }))),
-//   // query('.hamburger,:nth-child(2)',animate(1000, style({ opacity: 0  }))),
-//   // query('.hamburger,:nth-child(3)',animate(1000, style({ opacity: 0  }))),
-//
-// ]),
-// transition('close => open',[
-//   query('.hamburger,:nth-child(1)',animate(1000, style({ transform: 'translateX(-50px)' }))),
-//   // query('.hamburger,:nth-child(2)',animate(1000, style({ opacity: 0  }))),
-//   // query('.hamburger,:nth-child(3)',animate(1000, style({ opacity: 0  }))),
-//
-// ])
 export class NavbarComponent {
-  list_button=[
-    {'name':"Button1", _id:1},
-    {'name':"Button2", _id:2},
-    {'name':"Button3", _id:3},
-    {'name':"Button4", _id:4},
+  list_button = [
+    {'name': "Button1", _id: 1},
+    {'name': "Button2", _id: 2},
+    {'name': "Button3", _id: 3},
+    {'name': "Button4", _id: 4},
   ];
-
   burgerButtonOpen = 'close';
+
+  ngOnInit() {
+    this.burgerButtonOpen = 'open';
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const w = (event.target as Window).innerWidth;
+    if (w > 500) {
+      this.burgerButtonOpen = 'open';
+    }
+  }
+
   goAnimate() {
-    this.burgerButtonOpen =this.burgerButtonOpen == 'open'?'close':'open'  ;
+    this.burgerButtonOpen = this.burgerButtonOpen == 'open' ? 'close' : 'open';
     console.log(this.burgerButtonOpen)
   }
 
