32284216375829817d9e05bd0c1e9f38d986ae86
florian <sudreflorianpro@gmail.com>
2023-06-04 10:55:39 +0200

    fix(spec):remove spec file

--- src/app/coreDirective/image-carousel.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImageCarouselDirective } from './image-carousel.directive';
-
-describe('ImageCarouselDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImageCarouselDirective();
-    expect(directive).toBeTruthy();
-  });
-});
